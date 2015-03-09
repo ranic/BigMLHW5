c3847371cc7bd5fc59fe7d62f1525e04b245da00
vijay <vijay.iyengar713@gmail.com>
2015-03-09 05:17:33 -0500

    Full training with regularization

--- src/LR.java
+++ src/LR.java
@@ -13,19 +13,10 @@ public class LR {
     private static int maxPasses = 20;
     private static int trainingSize = 1000;
 
-    private static final String[] labelFilterArray = {"nl","el","ru","sl","pl","ca","fr","tr","hu","de","hr","es","ga","pt"};
-    private static final Set<String> labelFilter = new HashSet<String>(Arrays.asList(labelFilterArray));
+    private static final String[] labelArray = {"nl","el","ru","sl","pl","ca","fr","tr","hu","de","hr","es","ga","pt"};
+    private static final Set<String> labelFilter = new HashSet<String>(Arrays.asList(labelArray));
 
-    /* Map from label to a map from word position to a parameter weight for that position */
-    private static HashMap<String, HashMap<Integer, Double>> params = new HashMap<String, HashMap<Integer, Double>>();
-
-
-    /* Create a parameter HashMap for each label */
-    private static void initParams() {
-        for (String label : labelFilterArray) {
-            params.put(label, new HashMap<Integer, Double>());
-        }
-    }
+    private static HashMap<String, Map<Integer, Double>> params = new HashMap<String, Map<Integer, Double>>();
 
     /* Compute ID for each token. Generally unique */
     private static int computeTokenID(String token) {
@@ -34,11 +25,6 @@ public class LR {
     }
 
 
-    /**
-     * @param cur_doc
-     * @return
-     *  Vector of tokens from cur_doc
-     */
     static Vector<String> tokenizeDoc(String cur_doc) {
         String[] words = cur_doc.toLowerCase().split("\\s+");
         Vector<String> tokens = new Vector<String>();
@@ -72,26 +58,82 @@ public class LR {
 
 
     /* Computes and returns a map of label -> p(label) */
-    static HashMap<String, Double> computeP(Vector<String> doc) {
-        HashMap<String, Double> result = new HashMap<String, Double>();
-        HashMap<Integer, Double> B;
-        for (String label : labelFilterArray) {
-            double dotProd = 0.0;
-            B = params.get(label);
-            for (String token : doc) {
-                int id = computeTokenID(token);
-                if (B.containsKey(id)) {
-                    dotProd += B.get(id);
-                }
+    static double computeP(Vector<String> doc, Map<Integer, Double> B) {
+        double dotProd = 0.0;
+        for (String token : doc) {
+            int id = computeTokenID(token);
+            if (B.containsKey(id)) {
+                dotProd += B.get(id);
             }
-
-            result.put(label, sigmoid(dotProd));
         }
-        return result;
+        return sigmoid(dotProd);
     }
 
 
-    public static void LR() {
+
+    public static Map<Integer, Double> RegularizedLR(String label) {
+        double p;
+        HashMap<Integer, Integer> A = new HashMap<Integer, Integer>();
+        HashMap<Integer, Double> B = new HashMap<Integer, Double>();
+        for (int j = 0; j < vocabSize; j++) {
+            A.put(j, 0);
+            B.put(j, 0.0);
+        }
+        int id;
+        int k = 0;
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = br.readLine()) != null && line.length() != 0) {
+                // Each line is of the form Cat1,Cat2,...,CatK  w1 w2 w3 ... wN
+                String[] pair = line.split("\t");
+                Set<String> labels = filterLabels(pair[0].split(","));
+                Vector<String> tokens = tokenizeDoc(pair[1]);
+                HashMap<Integer, Integer> tokenIDs = new HashMap<Integer, Integer>();
+                k++;
+
+                // Build a table of ID -> count for tokens occurring in doc
+                for (String token : tokens) {
+                    id = computeTokenID(token);
+                    if (!tokenIDs.containsKey(id))
+                        tokenIDs.put(id, 0);
+
+                    tokenIDs.put(id, tokenIDs.get(id) + 1);
+                }
+
+                // Recompute p for the given label at the start of every example
+                p = computeP(tokens, B);
+
+                // Does this label appear in the training labels?
+                int y = (labels.contains(label)) ? 1 : 0;
+
+                for (int j = 0; j < vocabSize; j++) {
+                    if (tokenIDs.containsKey(j)) {
+                        double regularizationFactor = Math.pow(1.0 - 2 * learnRate * regularization, k - A.get(j));
+                        for (int i = 0; i < tokenIDs.get(j); i++) {
+                            B.put(j, B.get(j) + learnRate * (y - p));
+                            B.put(j, B.get(j) * regularizationFactor);
+                        }
+                        A.put(j, k);
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error:" + e.getMessage());
+            e.printStackTrace();
+        }
+
+        for (int j = 0; j < vocabSize; j++) {
+            double regularizationFactor = Math.pow(1.0 - 2 * learnRate * regularization, k - A.get(j));
+            B.put(j, B.get(j) * regularizationFactor);
+        }
+
+        return B;
+    }
+
+
+    /*public static void LR() {
         try {
             HashMap<String, Double> p;
             HashMap<Integer, Double> B;
@@ -130,12 +172,12 @@ public class LR {
             System.err.println("Error:" + e.getMessage());
         }
 
-    }
+    }*/
 
 
 
     public static void main(String[] args) {
-        if (args.length != 5) {
+        /*if (args.length != 5) {
             System.out.println("Invalid args.");
             return;
         }
@@ -145,7 +187,14 @@ public class LR {
         regularization = Double.valueOf(args[2]);
         maxPasses = Integer.valueOf(args[3]);
         trainingSize = Integer.valueOf(args[4]);
+*/
+        for (String label : labelArray)
+            params.put(label, RegularizedLR(label));
 
-        LR();
+        for (String label : labelArray) {
+            for (int i = 0; i < params.get(label).size(); i++) {
+                System.out.println(label + " " + i + " " + params.get(label).get(i));
+            }
+        }
     }
 }
